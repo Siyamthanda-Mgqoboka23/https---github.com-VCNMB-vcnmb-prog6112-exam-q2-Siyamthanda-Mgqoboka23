f591d83470b964464705cb3d27564530d375be57
Siyamthanda-Mgqoboka23 <128705712+Siyamthanda-Mgqoboka23@users.noreply.github.com>
2024-11-11 17:22:19 +0200

    Created

---
+++ src/main/java/com/mycompany/examquestion2/ExamQuestion2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.examquestion2;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class ExamQuestion2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
